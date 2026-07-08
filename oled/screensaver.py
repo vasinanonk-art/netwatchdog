73d256685366942a17bd1581f0fffeb1e003cea2
vasinanonk-art <vasinanon.k@gmail.com>
2026-07-09 00:20:17 +0700

    Add OLED blank screen saver window

--- oled/screensaver.py
+++ oled/screensaver.py
@@ -1,7 +1,19 @@
 """Burn-in protection helpers."""
 
-SHIFT_PATTERN = [(0, 0), (1, 0), (1, 1), (0, 1)]
+SHIFT_PATTERN = [(0, 0), (1, 0), (1, 1), (0, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
 
 
-def shift_for_tick(tick):
+def shift_for_tick(tick, enabled=True):
+    if not enabled:
+        return (0, 0)
     return SHIFT_PATTERN[tick % len(SHIFT_PATTERN)]
+
+
+def should_blank(now, last_activity, timeout_sec, blank_sec):
+    if timeout_sec <= 0 or blank_sec <= 0:
+        return False
+    elapsed = int(now - last_activity)
+    if elapsed < timeout_sec:
+        return False
+    cycle = timeout_sec + blank_sec
+    return (elapsed % cycle) >= timeout_sec
