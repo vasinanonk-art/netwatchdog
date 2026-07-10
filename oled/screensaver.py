0aa39cde03a5a0ed69e16cb35e50fff684d30ba2
vasinanonk-art <vasinanon.k@gmail.com>
2026-07-10 23:44:41 +0700

    Fix OLED pixel shift pattern

--- oled/screensaver.py
+++ oled/screensaver.py
@@ -1,6 +1,6 @@
 """Burn-in protection helpers."""
 
-SHIFT_PATTERN = [(0, 0), (1, 0), (1, 1), (0, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+SHIFT_PATTERN = [(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
 def shift_for_tick(tick, enabled=True):
